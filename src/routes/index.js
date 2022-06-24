5d3f5c61a902bbfe81f24efeafc9cac7fdb630c8
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-24 19:07:32 -0300

    feat: create index of routes

---
+++ src/routes/index.js
@@ -0,0 +1,15 @@
+const express = require('express');
+
+const router = express.Router();
+
+const loginRoutes = require('./loginRoutes');
+const usersRoutes = require('./usersRoutes');
+const categoriesRoutes = require('./categoriesRoutes');
+const postsRoutes = require('./postsRoutes');
+
+router.use('/login', loginRoutes);
+router.use('/users', usersRoutes);
+router.use('/categories', categoriesRoutes);
+router.use('/posts', postsRoutes);
+
+module.exports = router;
