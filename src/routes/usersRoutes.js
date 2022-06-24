3b2b65797a91f3d629a6f88ec12d03db77a368bd
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-24 19:06:57 -0300

    feat: create users routes

---
+++ src/routes/usersRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+
+const router = express.Router();
+
+const { validateUser } = require('../middlewares/userMiddleware');
+const { validateToken } = require('../middlewares/validateToken');
+
+const {
+  setNewUserController,
+  getAllUsersController,
+  getUserByIdController,
+  deleteUserController,
+} = require('../controllers/userController');
+
+router.post('/', validateUser, setNewUserController);
+router.get('/:id', validateToken, getUserByIdController);
+router.get('/', validateToken, getAllUsersController);
+router.delete('/me', validateToken, deleteUserController);
+
+module.exports = router;
