181bb88cbc5af3a26f264b302b020241a7bbf583
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-26 19:47:53 -0300

    fix: remove id in models

--- src/database/models/user.js
+++ src/database/models/user.js
@@ -1,10 +1,5 @@
 module.exports = (sequelize, DataTypes) => {
   const User = sequelize.define('User', {
-    id: {
-      type: DataTypes.INTEGER,
-      primaryKey: true,
-      autoIncrement: true,
-    },
     displayName: DataTypes.STRING,
     email: DataTypes.STRING,
     password: DataTypes.STRING,
