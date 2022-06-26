620c4570aae3b6fc1e44b0077a94216f741c6be0
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-26 13:43:02 -0300

    feat: create authenticate token | refactor: add try/catch in generate token

--- src/utils/jwt.js
+++ src/utils/jwt.js
@@ -13,17 +13,31 @@ const generateJWT = (
     id, displayName, email, image,
   },
 ) => {
-  const response = jwt.sign(
-    {
-      id, displayName, email, image,
-    },
-    JWT_TOKEN_SECRET,
-    jwtConfig,
-  );
+  try {
+    const response = jwt.sign(
+      {
+        id, displayName, email, image,
+      },
+      JWT_TOKEN_SECRET,
+      jwtConfig,
+    );
+    return response;
+  } catch (error) {
+    return { status: 500, message: 'Erro ao gerar token. Por favor, tente novamente.' };
+  }
+};
 
-  return response;
+const authenticateToken = (token) => {
+  try {
+    const validate = jwt.verify(token, JWT_TOKEN_SECRET);
+
+    return validate;
+  } catch (error) {
+    return { status: 401, message: 'Token expirado ou inválido' };
+  }
 };
 
 module.exports = {
   generateJWT,
+  authenticateToken,
 };
