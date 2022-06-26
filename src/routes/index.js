a82c3ef37e529f93e1da54d2f20cc855b56d9837
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-26 19:51:05 -0300

    feat: add users rotes in rotes

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,12 +3,12 @@ const express = require('express');
 const router = express.Router();
 
 const loginRoutes = require('./loginRoutes');
-// const usersRoutes = require('./usersRoutes');
+const usersRoutes = require('./usersRoutes');
 // const categoriesRoutes = require('./categoriesRoutes');
 // const postsRoutes = require('./postsRoutes');
 
 router.use('/login', loginRoutes);
-// router.use('/users', usersRoutes);
+router.use('/users', usersRoutes);
 // router.use('/categories', categoriesRoutes);
 // router.use('/posts', postsRoutes);
 
