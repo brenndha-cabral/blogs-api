a13becdd3eb13dadb6f8fa2a2352581eb7acc64c
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-26 20:23:46 -0300

    feat: add validateToken and getAllUsersController in users routes

--- src/routes/usersRoutes.js
+++ src/routes/usersRoutes.js
@@ -6,15 +6,16 @@ const {
   validateFieldsUser,
   validateRulesUser,
 } = require('../middlewares/validadeUser');
-// const { validateToken } = require('../middlewares/validateToken');
+const { validateToken } = require('../middlewares/validateToken');
 
 const {
   newUserController,
+  getAllUsersController,
 } = require('../controllers/userController');
 
 router.post('/', validateFieldsUser, validateRulesUser, newUserController);
 // router.get('/:id', validateToken, getUserByIdController);
-// router.get('/', validateToken, getAllUsersController);
+router.get('/', validateToken, getAllUsersController);
 // router.delete('/me', validateToken, deleteUserController);
 
 module.exports = router;
