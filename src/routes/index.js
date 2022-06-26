4ccd0726828eeacdec95c245a7a5a72b371f3950
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-26 17:24:43 -0300

    fix: comment some lines

--- src/routes/index.js
+++ src/routes/index.js
@@ -3,13 +3,13 @@ const express = require('express');
 const router = express.Router();
 
 const loginRoutes = require('./loginRoutes');
-const usersRoutes = require('./usersRoutes');
-const categoriesRoutes = require('./categoriesRoutes');
-const postsRoutes = require('./postsRoutes');
+// const usersRoutes = require('./usersRoutes');
+// const categoriesRoutes = require('./categoriesRoutes');
+// const postsRoutes = require('./postsRoutes');
 
 router.use('/login', loginRoutes);
-router.use('/users', usersRoutes);
-router.use('/categories', categoriesRoutes);
-router.use('/posts', postsRoutes);
+// router.use('/users', usersRoutes);
+// router.use('/categories', categoriesRoutes);
+// router.use('/posts', postsRoutes);
 
 module.exports = router;
