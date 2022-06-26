ca9f464399619b6ededbcbd64909400ddca352b7
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-26 13:01:58 -0300

    feat: create postCategory model

---
+++ src/database/models/postCategory.js
@@ -0,0 +1,32 @@
+module.exports = (sequelize, DataTypes) => {
+  const PostCategory = sequelize.define('PostCategory', {
+    postId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+    categoryId: {
+      type: DataTypes.INTEGER,
+      foreignKey: true,
+    },
+  }, {
+    timestamps: false,
+    underscored: true,
+  });
+
+  PostCategory.associate = (models) => {
+    models.Category.belongsToMany(models.BlogPost, {
+      as: 'blogPost',
+      through: PostCategory,
+      foreignKey: 'postId',
+      otherKey: 'categoryId',
+    });
+    models.BlogPost.belongsToMany(models.Category, {
+      as: 'categories',
+      through: PostCategory,
+      foreignKey: 'categoryId',
+      otherKey: 'postId',
+    });
+  };
+
+  return PostCategory;
+};
