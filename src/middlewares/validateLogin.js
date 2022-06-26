62ca7af4393a899d73bf187f13da15a1caba4326
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-26 14:33:36 -0300

    feat: create validateUserExist middleware

--- src/middlewares/validateLogin.js
+++ src/middlewares/validateLogin.js
@@ -21,6 +21,25 @@ const validateFieldsLogin = (req, res, next) => {
   return next();
 };
 
+const validateUserExist = async (req, res, next) => {
+  try {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({
+      where: { email, password },
+    });
+
+    if (!user) {
+      return res.status(400).json({ message: 'Pessoa usuária não encontrada. Por favor, tente novamente.' });
+    }
+
+    return next();
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
 module.exports = {
   validateFieldsLogin,
+  validateUserExist,
 };
