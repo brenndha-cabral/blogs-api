f89e72ad624e2fe1d0c98c5c8ecac9729eb1ef69
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-27 21:22:49 -0300

    feat: create getPostById service

--- src/services/postService.js
+++ src/services/postService.js
@@ -11,6 +11,19 @@ const getAllPosts = async () => {
   return response;
 };
 
+const getPostById = async (id) => {
+  const response = await BlogPost.findOne({
+    include: [
+      { model: User, as: 'user', attributes: { exclude: ['password'] } },
+      { model: Category, as: 'categories', through: { attributes: [] } },
+    ],
+    where: { id },
+  });
+
+  return response;
+};
+
 module.exports = {
   getAllPosts,
+  getPostById,
 };
