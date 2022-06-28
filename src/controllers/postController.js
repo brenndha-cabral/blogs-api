c4e0a84cb03b892b74baf2fc2a88d81ef80de559
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-27 21:22:38 -0300

    feat: create getPostByIdController

--- src/controllers/postController.js
+++ src/controllers/postController.js
@@ -14,6 +14,23 @@ const getAllPostController = async (req, res) => {
   }
 };
 
+const getPostByIdController = async (req, res) => {
+  try {
+    const { id } = req.params;
+
+    const post = await postService.getPostById(id);
+
+    if (!post) {
+      return res.status(404).json({ message: 'Post não existe' });
+    }
+
+    return res.status(200).json(post);
+  } catch (error) {
+    return res.status(500).json(error.message);
+  }
+};
+
 module.exports = {
   getAllPostController,
+  getPostByIdController,
 };
