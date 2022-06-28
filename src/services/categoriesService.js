45262234ea88f23d68f98d6994d1720ffd4fa08e
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-27 21:12:12 -0300

    feat: create setNewCategory service

--- src/services/categoriesService.js
+++ src/services/categoriesService.js
@@ -2,6 +2,16 @@ const { Category } = require('../database/models');
 
 const getAllCategories = () => Category.findAll();
 
+const setNewCategory = async (name) => {
+  const { id } = await Category.create({ name });
+
+  return {
+    id,
+    name,
+  };
+};
+
 module.exports = {
   getAllCategories,
+  setNewCategory,
 };
