22315b9c88787abe7f236caa70a0efd5f83c8c84
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-27 21:14:03 -0300

    feat: add categories and posts in index routes

--- src/routes/index.js
+++ src/routes/index.js
@@ -4,12 +4,12 @@ const router = express.Router();
 
 const loginRoutes = require('./loginRoutes');
 const usersRoutes = require('./usersRoutes');
-// const categoriesRoutes = require('./categoriesRoutes');
-// const postsRoutes = require('./postsRoutes');
+const categoriesRoutes = require('./categoriesRoutes');
+const postsRoutes = require('./postsRoutes');
 
 router.use('/login', loginRoutes);
 router.use('/users', usersRoutes);
-// router.use('/categories', categoriesRoutes);
-// router.use('/posts', postsRoutes);
+router.use('/categories', categoriesRoutes);
+router.use('/posts', postsRoutes);
 
 module.exports = router;
