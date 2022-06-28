1a9fdb57ec82ba49a44dda35e846de0fdf802263
brenndha-cabral <brenndha.cabral@yahoo.com>
2022-06-27 21:25:43 -0300

    feat: add getPostByQueryController in routes

--- src/routes/postsRoutes.js
+++ src/routes/postsRoutes.js
@@ -10,15 +10,15 @@ const { validateToken } = require('../middlewares/validateToken');
 // } = require('../middlewares/postMiddleware');
 // const { validateUserPost } = require('../middlewares/validateUser');
 const {
-//   newPostController,
   getAllPostController,
   getPostByIdController,
+  getPostByQueryController,
+//   newPostController,
 //   updatePostByIdController,
 //   removePostByIdController,
-//   getPostByQueryController,
 } = require('../controllers/postController');
 
-// router.get('/search', validateToken, getPostByQueryController);
+router.get('/search', validateToken, getPostByQueryController);
 router.get('/:id', validateToken, getPostByIdController);
 router.get('/', validateToken, getAllPostController);
 // router.post('/', validateToken, validatePost, validateCategoryId, newPostController);
